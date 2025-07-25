97f5abc38225396190f686b971c461066e38e1a1
NET001 <34703969+NET001@users.noreply.github.com>
2025-07-25 14:43:43 +0800

    提交代码

--- service/SurfWebDefault/Common/Quartz/JobConfig.cs
+++ service/SurfWebDefault/Common/Quartz/JobConfig.cs
@@ -8,6 +8,9 @@ namespace Common.Quartz
 {
     public class JobConfig
     {
+        /// <summary>
+        /// 同步任务执行
+        /// </summary>
         public int SequenceJobMinute { get; set; }
         public int ServerInfoCacheJobSecond { get; set; }
     }
